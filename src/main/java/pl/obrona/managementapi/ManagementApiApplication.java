6666a13eb93b36c2e122151a8b665d56dd9f19a1
Igor Kohsin <igor.kohsin@icloud.com>
2025-10-09 22:49:35 +0200

    CM-1 Change config, add TransactionScheduler

--- src/main/java/pl/obrona/managementapi/ManagementApiApplication.java
+++ src/main/java/pl/obrona/managementapi/ManagementApiApplication.java
@@ -2,8 +2,10 @@ package pl.obrona.managementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ManagementApiApplication {
 
     public static void main(String[] args) {
