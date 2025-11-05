9b40fbcbcabcb8666c03baeb27ee9dd9b3da2047
Igor Kohsin <igor.kohsin@icloud.com>
2025-11-05 12:41:52 +0100

    CM-4 Change config for exception handling

--- src/main/java/pl/obrona/managementapi/common/exception/handling/GlobalExceptionHandler.java
+++ src/main/java/pl/obrona/managementapi/common/exception/handling/GlobalExceptionHandler.java
@@ -3,9 +3,9 @@ package pl.obrona.managementapi.common.exception.handling;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import pl.obrona.managementapi.common.exception.ApiException;
 import pl.obrona.managementapi.common.exception.NotFoundException;
@@ -13,7 +13,7 @@ import pl.obrona.managementapi.common.exception.NotFoundException;
 import java.time.Clock;
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
